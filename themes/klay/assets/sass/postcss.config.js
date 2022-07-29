f17b8b4af40e0623334d64d401ff19940da5702b
Flex Zhong <chungzh07@gmail.com>
2022-07-29 12:20:05 +0800

    anchor

--- themes/klay/assets/sass/postcss.config.js
+++ themes/klay/assets/sass/postcss.config.js
@@ -1,25 +1,25 @@
-const themeDir = __dirname + '/../../';
+const themeDir = __dirname + "/../../";
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
-    content: [
-        './hugo_stats.json',
-        themeDir + '/hugo_stats.json',
-        'exampleSite/hugo_stats.json',
-    ],
-    safelist : [ /type/ ], // this helps to not purge type attributes, this is needed for the Typography plugin
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
-})
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
+  content: [
+    "./hugo_stats.json",
+    themeDir + "/hugo_stats.json",
+    "exampleSite/hugo_stats.json",
+  ],
+  safelist: [/type/], // this helps to not purge type attributes, this is needed for the Typography plugin
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  },
+});
 
-module.exports = {    
-    plugins: [
-        require('tailwindcss')(themeDir + 'assets/sass/tailwind.config.js'),
-        require('autoprefixer')({
-            path: [themeDir]
-        }),
-        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
-    ]
-}
+module.exports = {
+  plugins: [
+    require("tailwindcss")(themeDir + "assets/sass/tailwind.config.js"),
+    require("autoprefixer")({
+      path: [themeDir],
+    }),
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+  ],
+};
